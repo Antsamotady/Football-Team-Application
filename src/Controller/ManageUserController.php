2067966970a8aea437fff48d8e42eb60aa203414
Antsamotady <hasintsilavina@gmail.com>
2022-03-16 15:39:19 +0300

    Add user form

--- src/Controller/ManageUserController.php
+++ src/Controller/ManageUserController.php
@@ -5,10 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class ManageUserController extends AbstractController
 {
-    #[Route('/manage-user', name: 'manage_user')]
+    #[Route('/up-user', name: 'up_user')]
     public function index(): Response
     {
         return $this->render('manage_user/index.html.twig', [
@@ -35,12 +40,33 @@ class ManageUserController extends AbstractController
         ]);
     }
 
-    #[Route('/up-user', name: 'up_user')]
-    public function up(): Response
+    #[Route('/add-user', name: 'add_user')]
+    public function add(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('manage_user/up.html.twig', [
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+            $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('manage_user/add.html.twig', [
             'template_title' => 'Nouvel Utilisateur',
             'meth_name' => 'up',
+            'registrationForm' => $form->createView(),
         ]);
     }
 
