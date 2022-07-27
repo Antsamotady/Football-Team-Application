00cf3bf6f5cda6793a5c44a764b0b738e6ce8b45
Antsamotady <hasintsilavina@gmail.com>
2022-07-27 10:57:00 +0300

    Error handling on check keycompte

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -103,7 +103,7 @@ class SubscriptionController extends AbstractController
             'limitUser' => $limitUser,
             'limitEntity' => $limitEntity,
             'limitAnnonce' => $limitAnnonce,
-            "status" => 200,
+            'status' => 200,
         ], 200, []);
     }
 
