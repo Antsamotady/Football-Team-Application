62398633539fb40085fdf014636523d19f6a0c00
Antsamotady <hasintsilavina@gmail.com>
2023-08-29 06:42:15 +0300

    Start Student list page

---
+++ src/Data/StudentSearchData.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Data;
+
+class StudentSearchData
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+}
