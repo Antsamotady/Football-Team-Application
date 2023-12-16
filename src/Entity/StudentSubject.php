58b2b13a060e3ebc7b30e4e827609dd18d4abf4b
Antsamotady <hasintsilavina@gmail.com>
2023-12-16 09:38:07 +0300

    Cleanup StudentSubject from name

--- src/Entity/StudentSubject.php
+++ src/Entity/StudentSubject.php
@@ -14,9 +14,6 @@ class StudentSubject
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private $id;
-
-    #[ORM\Column(type: 'string', length: 255)]
-    private $name;
     
     #[ORM\ManyToOne(targetEntity: Student::class, inversedBy: 'subjects')]
     private $student;
@@ -35,18 +32,6 @@ class StudentSubject
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
     }
 
     public function getStudent(): ?Student
