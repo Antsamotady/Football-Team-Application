87e01b46cf78b475087d4414ae774bffc485a3ac
Your Name <you@example.com>
2026-01-23 07:20:50 +0000

    Add js to auto submit search name

---
+++ public/assets/js/custom/student/list.js
@@ -0,0 +1,27 @@
+function debounce(fn, delay) {
+    let timeout;
+    return function () {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => fn.apply(this, arguments), delay);
+    };
+}
+
+$('.student-search-name').on(
+    'input',
+    debounce(function () {
+        console.log('Auto submitting on input debounce...');
+        
+        $(this).closest('form').trigger('submit');
+    }, 500)
+);
+
+$(document).on('click', '[data-kt-search-element="clear"]', function (e) {
+    e.preventDefault();
+
+    const $form = $(this).closest('form');
+    const $input = $form.find('.student-search-name');
+
+    $input.val('');
+    $input.trigger('focus');
+    $form.trigger('submit');
+});
